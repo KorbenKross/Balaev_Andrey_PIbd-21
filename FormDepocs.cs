65b0e9ccf67f4ec9ffa9268fc9d44939e4aad4ef
KorbenKross <31703739+KorbenKross@users.noreply.github.com>
2017-12-09 23:37:11 +0400

    Add files via upload

---
+++ FormDepocs.cs
@@ -0,0 +1,182 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Lab2
+{
+    public partial class FormDepocs : Form
+    {
+        Depo depo;
+        Form1 form;
+        public FormDepocs()
+        {
+            InitializeComponent();
+            depo = new Depo(5);
+            for (int i = 1; i < 6; i++)
+            {
+                listBoxLevels.Items.Add("Уровень " + i);
+            }
+            listBoxLevels.SelectedIndex = depo.getCurrentLevel;
+   
+            Draw();
+        }
+
+        private void Draw()
+        {
+            if (listBoxLevels.SelectedIndex > -1)
+            {
+                Bitmap bmp = new Bitmap(pictureBoxParking.Width, pictureBoxParking.Height);
+                Graphics gr = Graphics.FromImage(bmp);
+                depo.Draw(gr, pictureBoxParking.Width, pictureBoxParking.Height);
+                pictureBoxParking.Image = bmp;
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            ColorDialog dialog = new ColorDialog();
+            if (dialog.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                var locomotive = new Locomotive(100, 4, 1000, dialog.Color);
+                int place = depo.PutLocoInDepo(locomotive);
+                Draw();
+                MessageBox.Show("Ваше место: " + place);
+            }
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            {
+                ColorDialog dialog = new ColorDialog();
+                if (dialog.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+                {
+                    ColorDialog dialogDop = new ColorDialog();
+                    if (dialogDop.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+                    {
+                        var locomotive = new CartLocomotive(100, 4, 1000, dialog.Color, true, true, true, dialogDop.Color);
+                        int place = depo.PutLocoInDepo(locomotive);
+                        Draw();
+                        MessageBox.Show("Ваше место: " + place);
+                    }
+                }
+
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            {
+                if (listBoxLevels.SelectedIndex > -1)
+                {//Прежде чем забрать машину, надо выбрать с какого уровня будем забирать
+                    string level = listBoxLevels.Items[listBoxLevels.SelectedIndex].ToString();
+                    if (maskedTextBox1.Text != "")
+                    {
+                        var locomotive = depo.GetLocoInDepo(Convert.ToInt32(maskedTextBox1.Text));
+
+                        Bitmap bmp = new Bitmap(pictureBoxTakeCar.Width, pictureBoxTakeCar.Height);
+                        Graphics gr = Graphics.FromImage(bmp);
+                        locomotive.setPosition(20, 50);
+                        locomotive.drawLocomotive(gr);
+                        pictureBoxTakeCar.Image = bmp;
+                        Draw();
+                    }
+                    else
+                    {//иначесообщаемобэтом
+                        MessageBox.Show("Извинте, на этом месте нет машины");
+                    }
+
+                }
+            }
+        }
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void buttonDown_Click(object sender, EventArgs e)
+        {
+            depo.LevelDown();
+            listBoxLevels.SelectedIndex = depo.getCurrentLevel;
+            Draw();
+
+        }
+
+        private void buttonUp_Click(object sender, EventArgs e)
+        {
+            depo.LevelUp();
+            listBoxLevels.SelectedIndex = depo.getCurrentLevel;
+            Draw();
+
+        }
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            form = new Form1();
+            form.AddEvent(AddLoco);
+            form.Show();
+
+        }
+
+        private void AddLoco(ITransport locomotive)
+        {
+            if (locomotive != null)
+            {
+                int place = depo.PutLocoInDepo(locomotive);
+                if (place > -1)
+                {
+                    Draw();
+                    MessageBox.Show("Ваше место: " + place);
+                }
+                else
+                {
+                    MessageBox.Show("Машину не удалось поставить");
+                }
+            }
+        }
+
+        private void сохранитьToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (saveFileDialog1.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                if (depo.SaveData(saveFileDialog1.FileName))
+                {
+                    MessageBox.Show("Сохранение прошло успешно", "",
+                        MessageBoxButtons.OK, MessageBoxIcon.Information);
+                }
+                else
+                {
+                    MessageBox.Show("Не сохранилось", "",
+                        MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+
+        }
+
+        private void загрузитьToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (openFileDialog1.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                if (depo.LoadData(openFileDialog1.FileName))
+                {
+                    MessageBox.Show("Загрузили", "",
+                        MessageBoxButtons.OK, MessageBoxIcon.Information);
+                }
+                else
+                {
+                    MessageBox.Show("Не загрузили", "",
+                        MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+                Draw();
+            }
+
+        }
+    }
+}
+
